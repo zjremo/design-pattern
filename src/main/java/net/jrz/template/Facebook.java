0f8c491039420e499a0edb2f05f96f57f38e5d61
zjremo <3106316195@qq.com>
2026-05-17 01:37:11 +0800

    feat(pattern): submit codes of template

---
+++ src/main/java/net/jrz/template/Facebook.java
@@ -0,0 +1,46 @@
+package net.jrz.template;
+
+public class Facebook extends Network {
+    public Facebook(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Override
+    public boolean logIn(String userName, String password) {
+        System.out.println("\nChecking user's parameters");
+        System.out.println("Name: " + this.userName);
+        System.out.print("Password: ");
+        for (int i = 0; i < this.password.length(); i++) {
+            System.out.print("*");
+        }
+        simulateNetworkLatency();
+        System.out.println("\n\nLogIn success on Facebook");
+        return true;
+    }
+
+    private void simulateNetworkLatency() {
+        try {
+            int i = 0;
+            System.out.println();
+            while (i < 10) {
+                System.out.print(".");
+                Thread.sleep(500);
+                ++i;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    @Override
+    boolean sendData(byte[] data) {
+        System.out.println("Message: '" + new String(data) + "' was posted on Facebook");
+        return true;
+    }
+
+    @Override
+    void logOut() {
+        System.out.println("User: '" + userName + "' was logged out from Facebook");
+    }
+}
